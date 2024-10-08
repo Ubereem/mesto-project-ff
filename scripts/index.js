11601c319b4473683f465cbfa108b770a48b6d36
Sh <amoralesxxx@mail.ru>
2024-10-08 14:09:43 +0300

    1

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,44 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+// Функция для создания карточки
+const placesList = document.querySelector('.places__list');
+
+function createCard(cardData, handleDelete) {
+    const cardTemplate = document.querySelector('#card-template').content.cloneNode(true);
+    const cardImage = cardTemplate.querySelector('.card__image');
+    const cardTitle = cardTemplate.querySelector('.card__title');
+    cardImage.src = cardData.link;
+    cardImage.alt = cardData.name;
+    cardTitle.textContent = cardData.name;
+
+    const deleteButton = cardTemplate.querySelector('.card__delete-button');
+    deleteButton.addEventListener('click', () => {
+        const cardElement = deleteButton.closest('.card');
+        handleDelete(cardElement); 
+    });
+
+    return cardTemplate;
+}
+
+function handleCardDelete(cardElement) {
+    cardElement.remove();
+}
+
+initialCards.forEach(cardData => {
+    const cardElement = createCard(cardData, handleCardDelete);
+    placesList.append(cardElement);
+});
+
+
+const addCard = document.querySelector('.profile__add-button')
+addCard.addEventListener('click', () => {
+    const newCardData = { 
+      name: 'Название новой карточки',
+      link: ''
+    };
+
+    const newCardElement = createCard(newCardData, handleCardDelete);
+  
+    placesList.prepend(newCardElement);
+  });
